e64b9107044a31b3a9225d76ea772d468a80f135
Jeremy Price <jeremy.price@careem.com>
2019-06-04 12:49:02 +0200

    Build all vort progs

--- build/build.bat
+++ build/build.bat
@@ -1,1 +1,13 @@
 echo Starting complete vort build.
+go get github.com/donomii/vort-fuse
+go build github.com/donomii/vort-fuse
+go get github.com/donomii/vort-launcher
+go build github.com/donomii/vort-launcher
+go get github.com/donomii/vort-nfs
+go build github.com/donomii/vort-nfs
+go get github.com/donomii/vort-pserver
+go build github.com/donomii/vort-pserver
+go get github.com/donomii/vort-ftprelay
+go build -o vort-ftprelay github.com/donomii/vort-ftprelay/vort/
+
+
