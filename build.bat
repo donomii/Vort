330764f4a80d06ea62dbc10fe43113f4ded482bb
Jeremy Price <donomii@gmail.com>
2019-06-23 12:08:23 +0200

    Build flags for windows

--- build.bat
+++ build.bat
@@ -2,15 +2,16 @@ echo Starting complete vort build.
 mkdir build
 export GIT_TERMINAL_PROMPT=1
 set -x GIT_TERMINAL_PROMPT 1
-go get github.com/donomii/vort-fuse
+set CPATH=C:\Program Files (x86)\WinFsp\inc\fuse
+go get -u github.com/donomii/vort-fuse
 go build github.com/donomii/vort-fuse
-go get github.com/donomii/vort-launcher
+go get -u github.com/donomii/vort-launcher
 go build github.com/donomii/vort-launcher
-go get github.com/donomii/vort-nfs
+go get -u github.com/donomii/vort-nfs
 go build github.com/donomii/vort-nfs
-go get github.com/donomii/vort-pserver
+go get -u github.com/donomii/vort-pserver
 go build github.com/donomii/vort-pserver
-go get github.com/donomii/vort-ftprelay
+go get -u github.com/donomii/vort-ftprelay
 go build -o vort-ftprelay github.com/donomii/vort-ftprelay/vort/
 
 
