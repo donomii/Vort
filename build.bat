5c1eac02272eb97b0f030ca85ad82a59d80c5316
Jeremy Price <jeremy.price@careem.com>
2019-06-23 11:41:55 +0200

    Better builds

--- build.bat
+++ build.bat
@@ -1,5 +1,11 @@
 echo Starting complete vort build.
 mkdir build
+cd build
+rm vort-fuse
+rm vort-launcher
+rm vort-nfs
+rm vort-pserver
+rm vort-ftprelay
 export GIT_TERMINAL_PROMPT=1
 set -x GIT_TERMINAL_PROMPT 1
 go get github.com/donomii/vort-fuse
