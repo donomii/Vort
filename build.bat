1fb2014b1af9a3616d3609fb50bc30dfd209e7ed
Jeremy Price <jeremy.price@careem.com>
2019-07-23 15:27:36 +0200

    Sync with git, and go clean before build

--- build.bat
+++ build.bat
@@ -8,13 +8,14 @@ rm vort-pserver
 rm vort-ftprelay
 export GIT_TERMINAL_PROMPT=1
 set -x GIT_TERMINAL_PROMPT 1
-go get github.com/donomii/vort-fuse
+go clean
+go get -u github.com/donomii/vort-fuse
 go build github.com/donomii/vort-fuse
-go get github.com/donomii/vort-launcher
+go get -u github.com/donomii/vort-launcher
 go build github.com/donomii/vort-launcher
-go get github.com/donomii/vort-nfs
+go get -u github.com/donomii/vort-nfs
 go build github.com/donomii/vort-nfs
-go get github.com/donomii/vort-pserver
+go get -u github.com/donomii/vort-pserver
 go build github.com/donomii/vort-pserver
-go get github.com/donomii/vort-ftprelay
+go get -u github.com/donomii/vort-ftprelay
 go build -o vort-ftprelay github.com/donomii/vort-ftprelay/vort/
