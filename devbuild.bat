0ae62a468a1912432bdde551755ac1a59ee58606
Jeremy Price <donomii@gmail.com>
2020-01-17 11:05:25 +0100

    Add fast build file

---
+++ devbuild.bat
@@ -0,0 +1,31 @@
+echo Starting complete vort build.
+apt install libxcursor-dev libx11-dev libxinerama-dev libglfw3-dev libxinerama1 libxi-dev
+sudo apt install libxcursor-dev libx11-dev libxinerama-dev libglfw3-dev libxinerama1 libxi-dev
+mkdir build
+cd build
+rm vort-fuse
+rm vortcmd
+rm vort-launcher
+rm vort-nfs
+rm vort-pserver
+rm vort-ftprelay
+export GIT_TERMINAL_PROMPT=1
+set -x GIT_TERMINAL_PROMPT 1
+go clean
+for /f %%i in ('date /T') do set BT="%%i"
+for /f %%i in ('git log --oneline -n 1') do set COMM="%%i"
+set CPATH=C:\Program Files (x86)\WinFsp\inc\fuse
+#go get -u github.com/donomii/hashare
+go build -ldflags "-X main.buildTime=%BT% -X main.commitHash=%COMM%" github.com/donomii/hashare/vortcmd
+#go get -u github.com/donomii/vort-fuse
+go build -ldflags "-X main.buildTime=%BT% -X main.commitHash=%COMM%" github.com/donomii/vort-fuse
+#go get -u github.com/donomii/vort-launcher
+go build -gcflags "-l -N" -ldflags "-X main.buildTime=%BT% -X main.commitHash=%COMM%" github.com/donomii/vort-launcher
+#go get -u github.com/donomii/vort-synchronise
+go build -ldflags "-X main.buildTime=%BT% -X main.commitHash=%COMM%" github.com/donomii/vort-synchronise
+#go get -u github.com/donomii/vort-nfs
+go build -ldflags "-X main.buildTime=%BT% -X main.commitHash=%COMM%" github.com/donomii/vort-nfs
+#go get -u github.com/donomii/vort-pserver
+go build -ldflags "-X main.buildTime=%BT% -X main.commitHash=%COMM%" github.com/donomii/vort-pserver
+#go get -u github.com/donomii/vort-ftprelay
+go build -ldflags "-X main.buildTime=%BT% -X main.commitHash=%COMM%" -o vort-ftprelay github.com/donomii/vort-ftprelay/vort/
