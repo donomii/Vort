eb83be7926fcf254ef58b8a425eefcab853b3654
Jeremy Price <jeremy.price@careem.com>
2020-12-02 19:06:05 +0100

    Better build, add webrelay

--- build.linux.bash
+++ build.linux.bash
@@ -3,27 +3,19 @@ apt install libxcursor-dev libx11-dev libxinerama-dev libglfw3-dev libxinerama1 
 sudo apt install libxcursor-dev libx11-dev libxinerama-dev libglfw3-dev libxinerama1 libxi-dev
 mkdir build
 cd build
-rm vort-fuse
-rm vort-launcher
-rm vort-nfs
-rm vort-pserver
-rm vort-ftprelay
+rm vort-fuse vort-launcher vort-nfs vort-pclient vort-pserver vort-ftprelay vort-webbrelay vort-synchronise
 export GIT_TERMINAL_PROMPT=1
 set -x GIT_TERMINAL_PROMPT 1
 go clean
 export BT=`date "+%Y-%m-%d%H:%M:%S"`
 export COMM=`git rev-parse HEAD`
-go get -u github.com/donomii/vort-fuse
-go get -u github.com/donomii/vort-launcher
-go get -u github.com/donomii/vort-nfs
-go get -u github.com/donomii/vort-pserver
-go get -u github.com/donomii/vort-ftprelay
-go get -u github.com/donomii/vort-synchronise
+go get -u github.com/donomii/vort-fuse github.com/donomii/vort-webrelay github.com/donomii/vort-launcher github.com/donomii/vort-nfs github.com/donomii/vort-pserver github.com/donomii/vort-ftprelay github.com/donomii/vort-synchronise
 go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM" -a github.com/donomii/vort-fuse 
-go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM -X main.pclient=true" -a -o vort-pclient github.com/donomii/vort-fuse 
-go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM"-a github.com/donomii/vort-launcher 
-go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM" -a github.com/donomii/vort-nfs 
-go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM" -a github.com/donomii/vort-pserver 
-go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM" -a -o vort-ftprelay github.com/donomii/vort-ftprelay/vort/ 
-go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM" -a github.com/donomii/vort-synchronise 
+go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM  -X main.pclient=true" -a -o vort-pclient github.com/donomii/vort-fuse 
+go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM  -X main.pclient=true" -a -o vort-webrelay github.com/donomii/vort-webrelay
+go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM"  github.com/donomii/vort-launcher 
+go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM"  github.com/donomii/vort-nfs 
+go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM"  github.com/donomii/vort-pserver 
+go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM"  -o vort-ftprelay github.com/donomii/vort-ftprelay/vort/ 
+go build -a -ldflags "-X main.buildTime=$BT -X main.commitHash=$COMM"  github.com/donomii/vort-synchronise 
 wait
